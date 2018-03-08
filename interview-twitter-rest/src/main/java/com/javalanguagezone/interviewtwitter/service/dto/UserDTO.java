ef87e583db3642068f6bd14b3f17c613ba19e77a
caiman90 <rejhanh@outlook.com>
2018-03-09 00:12:49 +0100

    Adding project files

---
+++ interview-twitter-rest/src/main/java/com/javalanguagezone/interviewtwitter/service/dto/UserDTO.java
@@ -0,0 +1,31 @@
+package com.javalanguagezone.interviewtwitter.service.dto;
+
+import com.javalanguagezone.interviewtwitter.domain.User;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+
+import static lombok.AccessLevel.PRIVATE;
+
+@Getter
+@NoArgsConstructor(access = PRIVATE)
+public class UserDTO {
+  private Long id;
+  private String username;
+  private String fullName;
+  private int numOfTweets;
+  private int numOfFollowers;
+  private int numOfFollowing;
+
+  public UserDTO(User user) {
+    this.id = user.getId();
+    this.username = user.getUsername();
+    this.fullName = user.getFirstName() + " " + user.getLastName();
+    this.numOfFollowers = user.getFollowers().size();
+    this.numOfFollowing = user.getFollowing().size();
+    this.numOfTweets = 0;
+  }
+
+  public void setNumOfTweets(int numOfTweets) {
+    this.numOfTweets = numOfTweets;
+  }
+}
---
+++ interview-twitter-ui/src/app/models/profile.model.ts
@@ -0,0 +1,8 @@
+export interface ProfileModel {
+  id: number,
+  username: string,
+  fullName: string,
+  numOfTweets:number,
+  numOfFollowers:number,
+  numOfFollowing:number
+}
